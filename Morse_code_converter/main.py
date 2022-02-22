20e7f586dd07c515b24f21fc45578959615226a5
huLasPet <kenwoodo@hotmail.com>
2022-02-22 10:34:49 +0100

    Morse code converter

--- Morse_code_converter/main.py
+++ Morse_code_converter/main.py
@@ -1,6 +1,7 @@
 from morse_converter import Converter
 
 if __name__ == "__main__":
+    print("Morse code converter, use characters that are found in the International Morse table.")
     message = input("Type in the message you want to see in Morse code: ")
     convert = Converter()
     print(convert.convert_to_morse(message))
