be053073a8c16a1eb78eb82a87d6aa8fa9d6d649
huLasPet <kenwoodo@hotmail.com>
2022-02-22 10:28:31 +0100

    Morse code converter

--- Morse_code_converter/main.py
+++ Morse_code_converter/main.py
@@ -1,4 +1,6 @@
 from morse_converter import Converter
 
-testing = Converter("Testing message")
-print(testing.convert())
+if __name__ == "__main__":
+    message = input("Type in the message you want to see in Morse code: ")
+    convert = Converter()
+    print(convert.convert_to_morse(message))
--- Morse_code_converter/morse_converter.py
+++ Morse_code_converter/morse_converter.py
@@ -1,7 +1,28 @@
+MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
+                   'C': '-.-.', 'D': '-..', 'E': '.',
+                   'F': '..-.', 'G': '--.', 'H': '....',
+                   'I': '..', 'J': '.---', 'K': '-.-',
+                   'L': '.-..', 'M': '--', 'N': '-.',
+                   'O': '---', 'P': '.--.', 'Q': '--.-',
+                   'R': '.-.', 'S': '...', 'T': '-',
+                   'U': '..-', 'V': '...-', 'W': '.--',
+                   'X': '-..-', 'Y': '-.--', 'Z': '--..',
+                   '1': '.----', '2': '..---', '3': '...--',
+                   '4': '....-', '5': '.....', '6': '-....',
+                   '7': '--...', '8': '---..', '9': '----.',
+                   '0': '-----', ', ': '--..--', '.': '.-.-.-',
+                   '?': '..--..', '/': '-..-.', '-': '-....-',
+                   '(': '-.--.', ')': '-.--.-'}
+
+
 class Converter:
     """Morse code converter. Takes a message and returns it in Morse code."""
-    def __init__(self, message):
-        self.message_list = list(message)
 
-    def convert(self):
-        return self.message_list
+    def __init__(self):
+        self.converted_list = []
+
+    def convert_to_morse(self, message):
+        for letter in message:
+            if letter.upper() in MORSE_CODE_DICT:
+                self.converted_list.append(MORSE_CODE_DICT[letter.upper()])
+        return self.converted_list
