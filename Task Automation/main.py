c24e685aaf8f9eaba58ac5c84620ce1110cce4cf
huLasPet <kenwoodo@hotmail.com>
2022-03-22 11:17:42 +0100

    Task Automation - extract TILL IDs

--- Task Automation/main.py
+++ Task Automation/main.py
@@ -1,11 +1,35 @@
+# TODO:
+#     Login will stay manual due to rolling admin password but it should:
+#     1: Enter the MongoDB and connect to the correct collection
+#     2: Get a list of TILLs where there are many stuck deltas
+#     3. Grab the TILL IDs from the screenshot of the SSH session
+#     4. Go through the IDs and delete the stuck deltas
+
+
 from PIL import Image
 import pytesseract
 import re
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Users\hulaspet\DEV\Python_shared_files\Tesseract\tesseract.exe"
 
-im = Image.open("test.PNG")
-data = pytesseract.image_to_string(im, config="--psm 6")
-ids = re.findall('"\d\d\d"|"\d\d\d\d"?', data)
-for id in ids:
-    print(f"TILL ID: {id}")
+
+class DataBaseCleanUp:
+    def __init__(self):
+        self.data = ""
+        self.ids = []
+        self.im = ""
+
+    def get_screenshot(self):
+        # Temp version for testing
+        self.im = Image.open("test.PNG")
+
+    def get_ids(self):
+        self.data = pytesseract.image_to_string(self.im, config="--psm 6")
+        self.ids = re.findall('"\d\d\d"|"\d\d\d\d"?', self.data)
+
+
+cleanup = DataBaseCleanUp()
+cleanup.get_screenshot()
+cleanup.get_ids()
+for till_id in cleanup.ids:
+    print(f"TILL ID: {till_id}")
