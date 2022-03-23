fad4d3d6d42c3767ccb81e8d26cdaca0a043d1c4
huLasPet <kenwoodo@hotmail.com>
2022-03-23 10:55:22 +0100

    Task Automation - works, may need minor tweaks

--- Task Automation/main.py
+++ Task Automation/main.py
@@ -1,48 +1,58 @@
-# TODO:
-#     Login will stay manual due to rolling admin password but it should:
-#     1: Enter the MongoDB and connect to the correct collection
-#     2: Get a list of TILLs where there are many stuck deltas
-#     3. Grab the TILL IDs from the screenshot of the SSH session
-#     4. Go through the IDs and delete the stuck deltas
-
-
-from PIL import Image
-import pytesseract
-import re, pyautogui, time, pyperclip
+import os
+import pyautogui
+import pyperclip
+import re
+import time
 import pygetwindow as gw
+import pytesseract
+from dotenv import load_dotenv
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Users\hulaspet\DEV\Python_shared_files\Tesseract\tesseract.exe"
+load_dotenv(r"C:\Users\hulaspet\DEV\Python_env\.env")
+
+
+def enter_db():
+    """Focuses on the window with the SSH session to the DB server
+    Enters the DB and lists all tills with their delta count."""
+    win = gw.getWindowsWithTitle(os.getenv('focus_window'))[0]
+    win.activate()
+    win.maximize()
+    pyautogui.hotkey('ctrl', 'c')
+    pyautogui.write('clear\n')
+    pyautogui.write('mongo\n')
+    pyautogui.write(os.getenv('dfm'))
+    pyautogui.press('enter')
+    time.sleep(1)
+    pyperclip.copy(os.getenv('list_tills'))
+    pyautogui.click(button='right')
+    pyautogui.press('enter')
 
 
 class DataBaseCleanUp:
+    """Handles the daily DB cleanup that was done manually before."""
     def __init__(self):
         self.data = ""
         self.ids = []
         self.im = ""
 
-    def enter_db(self):
-        win = gw.getWindowsWithTitle('hulaspet@A-241Y0F3: ~')[0]
-        win.activate()
-        win.maximize()
-        pyautogui.write('mongo\n')
-        pyautogui.write('use dfm\n')
-        time.sleep(1)
-        pyperclip.copy(r'db.dfm_nodeBatchList.aggregate([{"$group" : {_id:"$nodeId", count:{$sum:1}}},{$sort:{"count":-1}}])')
-        pyautogui.click(button='right')
-        pyautogui.press('enter')
-
     def get_screenshot(self):
-        # Temp version for testing
-        self.im = Image.open("test.PNG")
+        """Creates a screenshot of the listed tills."""
+        self.im = pyautogui.screenshot()
+        time.sleep(1)
 
     def get_ids(self):
+        """Gets the text from the screenshot and finds all till IDs."""
         self.data = pytesseract.image_to_string(self.im, config="--psm 6")
         self.ids = re.findall('"\d\d\d"|"\d\d\d\d"?', self.data)
 
 
+
 cleanup = DataBaseCleanUp()
-cleanup.enter_db()
+enter_db()
 cleanup.get_screenshot()
 cleanup.get_ids()
 for till_id in cleanup.ids:
-    print(f"TILL ID: {till_id}")
+    pyperclip.copy(f'db.dfm_nodeBatchList.deleteMany({{"nodeId": {till_id}}})')
+    pyautogui.click(button='right')
+    pyautogui.press('enter')
+    time.sleep(0.5)
