ded2c0f383bfb10cdb71d847a4d78fb3b28e83bb
huLasPet <kenwoodo@hotmail.com>
2022-04-11 08:13:25 +0200

    Added enter_db() and get_count() to the class as a staticmethod

--- Task Automation/main.py
+++ Task Automation/main.py
@@ -11,33 +11,6 @@ pytesseract.pytesseract.tesseract_cmd = r"C:\Users\hulaspet\DEV\Python_shared_fi
 load_dotenv(r"C:\Users\hulaspet\DEV\Python_env\.env")
 
 
-def enter_db():
-    """Focuses on the window with the SSH session to the DB server
-    Enters the DB and lists all tills with their delta count."""
-    win = gw.getWindowsWithTitle(os.getenv('focus_window'))[0]
-    win.activate()
-    win.maximize()
-    pyautogui.hotkey('ctrl', 'c')
-    pyautogui.write('clear\n')
-    pyautogui.write('mongo\n')
-    pyautogui.write(os.getenv('dfm'))
-    pyautogui.press('enter')
-    time.sleep(1)
-    pyperclip.copy(os.getenv('list_tills'))
-    pyautogui.click(button='right')
-    pyautogui.press('enter')
-    time.sleep(2)
-    pyautogui.write('it\n')
-    time.sleep(2)
-
-
-def get_count():
-    """Get a count of all deltas in the DB."""
-    pyperclip.copy(os.getenv('dfm_number'))
-    pyautogui.click(button='right')
-    pyautogui.press('enter')
-
-
 class DataBaseCleanUp:
     """Gets the till IDs for the daily cleanup."""
     def __init__(self):
@@ -55,14 +28,42 @@ class DataBaseCleanUp:
         self.data = pytesseract.image_to_string(self.im, config="--psm 6")
         self.ids = re.findall('"\d\d\d"|"\d\d\d\d"?', self.data)
 
-
-cleanup = DataBaseCleanUp()
-enter_db()
-cleanup.get_screenshot()
-cleanup.get_ids()
-for till_id in cleanup.ids:
-    pyperclip.copy(f'db.dfm_nodeBatchList.deleteMany({{"nodeId": {till_id}}})')
-    pyautogui.click(button='right')
+    @staticmethod
+    def enter_db():
+        """Focuses on the window with the SSH session to the DB server
+        Enters the DB and lists all tills with their delta count."""
+    win = gw.getWindowsWithTitle(os.getenv('focus_window'))[0]
+    win.activate()
+    win.maximize()
+    pyautogui.hotkey('ctrl', 'c')
+    pyautogui.write('clear\n')
+    pyautogui.write('mongo\n')
+    pyautogui.write(os.getenv('dfm'))
     pyautogui.press('enter')
     time.sleep(1)
-get_count()
+    pyperclip.copy(os.getenv('list_tills'))
+    pyautogui.click(button='right')
+    pyautogui.press('enter')
+    time.sleep(2)
+    pyautogui.write('it\n')
+    time.sleep(2)
+
+    @staticmethod
+    def get_count():
+        """Get a count of all deltas in the DB."""
+        pyperclip.copy(os.getenv('dfm_number'))
+        pyautogui.click(button='right')
+        pyautogui.press('enter')
+
+
+if __name__ == "__main__":
+    cleanup = DataBaseCleanUp()
+    cleanup.enter_db()
+    cleanup.get_screenshot()
+    cleanup.get_ids()
+    for till_id in cleanup.ids:
+        pyperclip.copy(f'db.dfm_nodeBatchList.deleteMany({{"nodeId": {till_id}}})')
+        pyautogui.click(button='right')
+        pyautogui.press('enter')
+        time.sleep(1)
+    cleanup.get_count()
